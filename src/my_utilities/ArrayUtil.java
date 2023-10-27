80412559deb0592d6666dea07f471077f18efa92
product077 <products0707@gmail.com>
2023-10-26 22:21:38 -0400

    Methods, method overloading, var args

--- src/my_utilities/ArrayUtil.java
+++ src/my_utilities/ArrayUtil.java
@@ -1,4 +1,56 @@
 package my_utilities;
 
 public class ArrayUtil {
+
+    public static int sumOfArray ( int [] arr) {
+        int sum = 0;
+
+        for ( int eachNum : arr) {
+            sum += eachNum;
+        }
+        return sum;
+    }
+
+
+    public static int minInArray (int [] arr) {
+
+        int min = arr[0];
+
+        for ( int eachElem : arr) {
+            if( eachElem < min) {
+                min = eachElem;
+            }
+        }
+        return min;
+    }
+
+
+    public static int maxInArray (int [] arr) {
+
+        int max = arr[0];
+
+        for ( int eachElem : arr) {
+            if( eachElem > max) {
+                max = eachElem;
+            }
+        }
+        return max;
+    }
+
+    /**
+     * This method accepts an int array and an int numb
+     * Then check if the number is in the array
+     * If yes, return true, otherwise return false
+     */
+    public static boolean containsInArray (int [] arr, int num) {
+        // [1, 3, 45, 57, 54, 57]  - 57
+        for ( int eachElem : arr) {
+            if (eachElem == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
