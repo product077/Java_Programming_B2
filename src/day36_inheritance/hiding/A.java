5498ca81f7e38d1c7b1a35d8bcbd041dcf55b85c
product077 <products0707@gmail.com>
2023-11-22 18:36:58 -0500

    Inheritance,hiding

---
+++ src/day36_inheritance/hiding/A.java
@@ -0,0 +1,14 @@
+package day36_inheritance.hiding;
+
+public class A {
+
+
+    public void instanceMethod () {
+        staticMethod();
+    }
+
+    public static void staticMethod () {
+        System.out.println("Static from PARENT");
+    }
+
+}
