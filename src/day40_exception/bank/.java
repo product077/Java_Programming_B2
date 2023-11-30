595305c7439eea1dbb162e0801220bab273ac163
product077 <products0707@gmail.com>
2023-11-29 22:29:38 -0500

    Exceptions 1

--- src/day40_exception/bank/.java
+++
@@ -1,7 +0,0 @@
-//package day40_exception.bank;
-////TODO: manage the code to give the CUSTOM exception
-//public class NotEnoughBalanceException extends RuntimeException{
-//    public NotEnoughBalanceException(String message) {
-//        super(message);
-//    }
-//}
